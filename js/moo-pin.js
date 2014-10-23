694967572fe83290e9265acae3ac251a3b27821f
dsiddharth2 <dsiddharth2@gmail.com>
2014-10-23 10:54:09 +0530

    Made the Changs to javascript file

--- js/moo-pin.js
+++ js/moo-pin.js
@@ -6,66 +6,51 @@ var MooPin = {};
 
 		options: {
 			container 	: '.moo-main',		// Main Container for the display of items
-			gutter		: '20',				// The Sapce between the columns displayed
+			gutter		: 20,				// The Sapce between the columns displayed
 		},
 
 		initialize: function(options){
 			this.setOptions(options);
-			this._load();
+			this.element = $(this.options.container);
+
+			this.renderPins(); // Code to show the pins
 		},
 
-		// Private Methods
-		_load: function() {
-	        this.element = $(this.container);
+		renderPins: function() {
+			var self = this;
+			
+			var // Get the column size
+				column_size = self.element.getElement('.item').getSize(),
 
-	        var column_size = this.element.getElement('.item').getSize(), // Get the column size
-	        	widthParentTiles = this.element.getElement('.tiles').getSize().x + 20, // Get the total width
-	        	array_Height = null, // Array for storing the heights of each elements
+				// Get the total width of the container
+				totalContainerWidth = self.element.getElement('.tiles').getSize().x + 20,
 
-	        // Calculate total number of columns that can be fit into the tile width
-        	this.totalColumns = (widthParentTiles / ( column_size.x + this.gutter )).toInt();
+				// Calculate total number of columns that can be fit into the tile width
+				totalColumns = (totalContainerWidth / ( column_size.x + this.options.gutter )).toInt(),
 
-        	// Array of heights to store each column heights
-	        var array_Height = new Array(totalElementsCanFit);
-	        var item_elements = main_contet.getElements('.item');
-	        var theIndex = 0;
-	        var tiles_position = main_contet.getElement('.tiles').getPosition(main_contet);
+				// Array of heights to store each column heights
+				array_Height = new Array(totalColumns);
 
-	        var leftPosition = tiles_position.x;
-	        var topPosition = tiles_position.y;
-	        var item_size = 0;
-	        var min_value = 0;
+			// Initialze array elements with zero
+			for (var i = 0; i < array_Height.length ; i++) {
+				array_Height[i] = 0;
+			}
+			
+			// Decide the position of elements
+			self.element.getElements('.item').each(function(item, index) {				
+				min_value = array_Height.min();
+				item_size = item.getSize();
+				topPosition = min_value;
+				index_of_min = array_Height.indexOf(min_value);				
+				leftPosition = (item_size.x + self.options.gutter) * (index_of_min);
+				array_Height[index_of_min] = array_Height[index_of_min] + 20 + item_size.y;
 
-	        for (var i = 0; i < array_Height.length ; i++) {
-	          array_Height[i] = 0;
-	        }
-
-	        item_elements.each(function(item, index) {
-	          theIndex = index % totalElementsCanFit;
-	          item_size = item.getSize();
-
-	          min_value = array_Height.min();
-	          index_of_min = array_Height.indexOf(min_value);
-
-	          topPosition = min_value;
-	          leftPosition = (item_size.x + col_margin) * (index_of_min);
-	          array_Height[index_of_min] = array_Height[index_of_min] + 20 + item_size.y;
-
-	          console.log({
-	            top: topPosition + 'px',
-	            left: leftPosition + 'px',
-	            position: 'absolute'
-	          });
-
-	          item.setStyles({
-	            top: topPosition + 'px',
-	            left: leftPosition + 'px',
-	            position: 'absolute'
-	          });
-	        });
-
-
-
-		}.protect(),
+				item.setStyles({
+					top: topPosition + 'px',
+					left: leftPosition + 'px',
+					position: 'absolute'
+				});
+			});
+		}
 	});
 })(document.id);
